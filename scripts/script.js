f357c4a41ccf1a20e3aa9ec8abf040194deda959
derushio <narumi18wa@gmail.com>
2018-10-05 18:59:36 +0900

    todo

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
-window.loadText = (id, path) => {
+window.downloadSJIS = (path) => {
+    const filename = path.replace(/^.*\//, '');
+
     const xhr = new XMLHttpRequest();
     xhr.open('GET', path, true);
-    xhr.overrideMimeType('text/plain; charset=shift_jis')
     xhr.onload = () => {
-        document.getElementById(id).innerText = xhr.responseText;
+        // TODO: sjis & CRLFに変換
+        const blob = new Blob([ xhr.responseText ], { 'type' : 'text/plain' });
+
+        if (window.navigator.msSaveBlob) {
+            // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+            window.navigator.msSaveBlob(blob, filename);
+        } else {
+            const url = window.URL.createObjectURL(blob);
+            var download = document.createElement('a');
+            document.body.appendChild(download);
+            download.href = url;
+            download.download = filename;
+            download.click();
+            document.body.removeChild(download);
+        }
     };
     xhr.send(null);
 }
