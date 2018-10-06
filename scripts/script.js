99d826ec85519fdb879a0f0c57c19848c03f3e7b
derushio <narumi18wa@gmail.com>
2018-10-07 00:28:13 +0900

    up

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,33 @@ window.downloadSJIS = (path) => {
     };
     xhr.send(null);
 }
+
+downloadSJISNoComment = (path) => {
+    const filename = path.replace(/^.*\//, '');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.onload = () => {
+        const noComment = xhr.response.replace(/\/\/.*\n/g, '');
+        const crlf = noComment.replace(/\n/g, '\r\n');
+        const stringArray = Encoding.stringToCode(crlf);
+        const sjisArray = Encoding.convert(stringArray, 'shift_jis');
+        const buffer = new Uint8Array(sjisArray);
+        const blob = new Blob([ buffer ], { 'type' : 'text/plain;charset=shift_jis;' });
+
+        if (window.navigator.msSaveBlob) {
+            // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+            window.navigator.msSaveBlob(blob, filename);
+        } else {
+            const url = window.URL.createObjectURL(blob);
+            const download = document.createElement('a');
+            download.style.display = 'none';
+            document.body.appendChild(download);
+            download.href = url;
+            download.download = filename;
+            download.click();
+            document.body.removeChild(download);
+        }
+    };
+    xhr.send(null);
+}
